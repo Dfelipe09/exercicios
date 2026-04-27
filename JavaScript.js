624ab86276da0bfc2f70848107b95180736961a7
I1HSA <thiago777diassilva@icloud.com>
2026-04-27 09:10:30 -0300

    comitei inicial

---
+++ JavaScript.js
@@ -0,0 +1,26 @@
+let valor;
+valor = Number(prompt("Insira o valor da sua compra:"));
+    
+if (isNaN(valor)) {
+    alert("Você não digitou um número!");
+} else { 
+    if (valor >= 150) {
+        let desc;
+        desc = Number(valor * 0.02); 
+        let vCdesc;
+        vCdesc = Number(valor * 0.98);
+
+        alert("Parabéns! Você recebeu um desconto de " + desc.toFixed(2) + " reais");
+        alert("O valor do produto com desconto é igual a " + vCdesc.toFixed(2) + " reais");
+
+    } else {
+        let cashback;
+        cashback = Number(valor * 0.02);
+
+        if (valor == 1) {
+            alert("O valor do produto é igual a " + valor + " real, com isso você recebeu um cashback de " + cashback.toFixed(2) + " reais");
+        } else {
+            alert("O valor do produto é igual a " + valor + " reais, com isso você recebeu um cashback de " + cashback.toFixed(2) + " reais");
+        }
+    }
+}
