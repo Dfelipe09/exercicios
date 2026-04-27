a3652b80d8b0c97a3b6e35267acf135c740db98d
I1HSA <thiago777diassilva@icloud.com>
2026-04-27 09:19:07 -0300

    ex2

--- JavaScript.js
+++ JavaScript.js
@@ -1,26 +1,13 @@
-let valor;
-valor = Number(prompt("Insira o valor da sua compra:"));
-    
-if (isNaN(valor)) {
-    alert("Você não digitou um número!");
-} else { 
-    if (valor >= 150) {
-        let desc;
-        desc = Number(valor * 0.02); 
-        let vCdesc;
-        vCdesc = Number(valor * 0.98);
+let altura
+altura = Number(prompt("insira a sua senha em metros: "));
 
-        alert("Parabéns! Você recebeu um desconto de " + desc.toFixed(2) + " reais");
-        alert("O valor do produto com desconto é igual a " + vCdesc.toFixed(2) + " reais");
+if (altura >= 1.40) {
 
-    } else {
-        let cashback;
-        cashback = Number(valor * 0.02);
+    alert ("você pode entrar no brinquedo.")
 
-        if (valor == 1) {
-            alert("O valor do produto é igual a " + valor + " real, com isso você recebeu um cashback de " + cashback.toFixed(2) + " reais");
-        } else {
-            alert("O valor do produto é igual a " + valor + " reais, com isso você recebeu um cashback de " + cashback.toFixed(2) + " reais");
-        }
-    }
 }
+else { 
+ 
+    alert("altura insuficiente.")
+
+};
